1315e1ccfff1acd0216a25751430b6787ff684c6
Tufem Gift Items <Laughteriremide@gmail.com>
2025-02-06 15:09:46 +0100

    my changes2

--- src/Components/Slide.jsx
+++ src/Components/Slide.jsx
@@ -1,9 +1,13 @@
+import { FaQuoteLeft } from "react-icons/fa";
+
 function Slide(props) {
   return (
     <div className="slide">
       <div className="row">
         <div className="col">
-          <h2>"</h2>
+          <h2 className="pbottom">
+            <FaQuoteLeft className="orange" />
+          </h2>
         </div>
       </div>
       <div className="row">
@@ -16,8 +20,8 @@ function Slide(props) {
           <div className="passport-div">
             <img src={props.img} alt="slide image" className="passport-img" />
           </div>
-          <p className="">{props.para1}</p>
-          <p className="">{props.para2}</p>
+          <p className="para1">{props.para1}</p>
+          <p className="orange ">{props.para2}</p>
         </div>
       </div>
     </div>
